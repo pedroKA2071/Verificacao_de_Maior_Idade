e73274e1e021b263af447c0d0f5a44b8728373f1
Pedro Henrique Gaspar <mortadela22112005@gmail.com>
2023-08-15 09:47:06 -0300

    maio ou mero de idade em C#

---
+++ Verificação de Maior Idade/Verificação de Maior Idade/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Verificação_de_Maior_Idade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            float idade;
+
+            Console.WriteLine("Qual a sua idade?");
+            idade = float.Parse(Console.ReadLine());
+
+            if (idade >= 18) 
+            {
+
+                Console.WriteLine("Voce é maior de idade!");
+
+
+            }
+            else
+            {
+                Console.WriteLine("Voce é menor de idade!");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
